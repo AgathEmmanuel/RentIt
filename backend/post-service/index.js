214619935783130e3822cbc23047cd9d57e19065
agathemmanuel <agathemmanuel00@gmail.com>
2022-04-05 11:06:08 +0530

    Added a basic post api endpoint

--- backend/post-service/index.js
+++ backend/post-service/index.js
@@ -1,7 +1,19 @@
 
 const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.json());
+
+
+/*
+------- end points
+
+/post   POST   {title: string}   create a new post
+/post   GET           -          get all posts
+
+*/
 
 const post = {};
 
@@ -12,7 +24,10 @@ app.get('/post',(req,res) => {
 });
 
 app.post('/post',(req,res) => {
-
+    const postId = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    post[postId]= { postId, title };
+    res.status(201).send(post[postId]);
 });
 
 
