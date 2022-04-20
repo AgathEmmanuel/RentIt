9a723e87b233bf0093863f12c52a229a9156e0e7
agathemmanuel <agathemmanuel00@gmail.com>
2022-04-20 07:37:26 +0530

    Adding a shared custom pacakage and publishing to npm

---
+++ backend/shared-custom-package/index.ts
@@ -0,0 +1,17 @@
+interface FruitCount {
+  apple: number;
+  orange: number;
+  grapes: number;
+  cherry: number;
+}
+
+const bucket1Count: FruitCount = {
+  apple: 12,
+  orange: 10,
+  grapes: 2,
+  cherry: 9,
+};
+
+console.log(bucket1Count);
+
+export default bucket1Count;
