c8050f686ebe9f2d5773ae51f4ae386c95087854
agathemmanuel <agathemmanuel00@gmail.com>
2022-04-20 09:44:59 +0530

    Updating the shared custom pakage

--- backend/shared-custom-package/src/index.ts
+++ backend/shared-custom-package/src/index.ts
@@ -5,6 +5,7 @@ export * from "./error/database-connection-error";
 export * from "./error/route-not-found-error";
 export * from "./error/errorInterface";
 export * from "./error/request-validation-error";
+export * from "./error/existing-user-error";
 
 export * from "./middleware/current-user-handler";
 export * from "./middleware/request-validater";
