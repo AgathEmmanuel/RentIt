9396a7ca24cc4a0368726e13106dc9234bd0ee3c
agathemmanuel <agathemmanuel00@gmail.com>
2022-05-26 08:05:32 +0530

    started with the creation of api for cloudbucket operator

--- operations/operators/cloudbucket/main.go
+++ operations/operators/cloudbucket/main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	cloudbucketv1alpha1 "github.com/AgathEmmanuel/RentIt/operations/operators/cloudbucket/api/v1alpha1"
+	"github.com/AgathEmmanuel/RentIt/operations/operators/cloudbucket/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(cloudbucketv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.BucketStorageReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "BucketStorage")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
