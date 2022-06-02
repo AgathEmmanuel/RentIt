91f050bba11aa1e80bf57ff63cb8c8f1de9df25a
agathemmanuel <agathemmanuel00@gmail.com>
2022-06-02 13:27:26 +0530

    Adding axios to next client

--- frontend/rentit-client-next/README.md
+++ frontend/rentit-client-next/README.md
@@ -9,6 +9,12 @@ npm init -y
 
 npm install react react-dom next
 
+npm run start
+
+npm install bootstrap
+
+npm install axios
+
 ```
 
 
--- frontend/rentit-client-next/pages/user/signup.js
+++ frontend/rentit-client-next/pages/user/signup.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+
+
+
 export default () => {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const onSubmit = async event => {
+      event.preventDefault(); // to make sure form does not try to submit itself in browser
+
+      const response = await axios.post('/api/user/signup', { userEmail, userPassword});
+      //console.log(userEmail,userPassword);
+      console.log(response.data);
+
+  };
+
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
           <label>Email Address</label>
-          <input className="form-control" />
+          <input value={userEmail} onChange={e => setUserEmail(e.target.value)} className="form-control" />
       </div>
       <div className="form-group">
           <label>Password</label>
-          <input type="password" className="form-control" />
+          <input value={userPassword} onChange={e => setUserPassword(e.target.value)} type="password" className="form-control" />
       </div>
       <button className="btn btn-primary"> Sign up</button>
     </form>
