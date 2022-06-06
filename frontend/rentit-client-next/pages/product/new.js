40efe94b05eaf368a3991782f8314a683bdf152a
agathemmanuel <agathemmanuel00@gmail.com>
2022-06-06 13:43:28 +0530

    Adding proper formating sanitizing to input price numbers

--- frontend/rentit-client-next/pages/product/new.js
+++ frontend/rentit-client-next/pages/product/new.js
@@ -1,15 +1,36 @@
+import { useState } from "react";
+
+
+
 const NewProduct = () => {
+
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+
+    // get triggered right when user clicks inside the input box and then outside
+    const actionOnBlur = () => {
+        const value = parseFloat(price);
+
+        if (isNaN(value)) {
+            return;
+        }
+        setPrice(value.toFixed(2));
+    }
+
     return (
     <div>
         <h1>Rent out a Product  </h1>
         <form>
             <div className="form-group">
                 <label>Name</label>
-                <input className="form-control" />
+                <input value= {name} onChange={(e) => setName(e.target.value)} className="form-control" />
             </div>
             <div className="form-group">
                 <label>Prize</label>
-                <input className="form-control" />
+                <input 
+                value={price} 
+                onBlur={actionOnBlur}
+                onChange={(e) => setPrice(e.target.value)} className="form-control" />
             </div>
             <button className="btn btn-primary">Create</button>
         </form>
