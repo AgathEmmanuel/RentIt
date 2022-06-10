3bb131c39b76c5b960260209b5e3ed2baf199fe5
agathemmanuel <agathemmanuel00@gmail.com>
2022-06-10 07:26:29 +0530

    Adding card to rentins page as well

--- frontend/rentit-client-next/pages/rentit/index.js
+++ frontend/rentit-client-next/pages/rentit/index.js
@@ -1,11 +1,19 @@
 const RentInsList = ({ rentins }) => {
     return <div>
         {rentins.map(rentin => {
-            return <li key={rentin.id}>
-                {rentin.product.productName} --- {rentin.status}
-            </li>
+    return <div key={rentin.id}>
+             <div className="w-75 mb-4">
+             <div className="card" >
+            <div className="card-body">
+            <h5 className="card-title">{rentin.product.productName}</h5>
+            <h4 className="card-title">Status: {rentin.status}</h4>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+            </div>
+            </div>
         })}
-    </div>;
+</div>
 }
 
 RentInsList.getInitialProps = async (context,client) => {
